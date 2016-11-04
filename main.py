fca1c2c21eb254ec93dc53440003ea82b5091ea4
u2daniel <daniel.rodrighes@gmail.com>
2016-11-04 12:49:48 -0300

    modificado

--- main.py
+++ main.py
@@ -3,9 +3,11 @@ from PyQt4.QtCore import *
 import sys
 import pysnmp
 import os
+import threading
 from socket import *
 from netaddr import *
 from pysnmp.entity.rfc3413.oneliner import cmdgen
+from pysnmp.hlapi import  getCmd
 
 
 
@@ -44,25 +46,6 @@ txtDisplay.setStyleSheet("QTextEdit {background-color:black;color:white}")
 
 
 
-
-
-
-
-def check_online(host):
-
-    resposta= os.system("ping -c 1 %s" % host)
-
-    if resposta == 0:
-        pingstatus = "<span style='color:#80ff00;font-weight:bold'>Ativo  ;)</span>"
-    else:
-        pingstatus = "<span style='color:#ff0000;font-weight:bold'>Inativo  :(</span>"
-
-    return pingstatus
-
-
-
-
-
 def escrever(redes):
     txtDisplay.clear()
 
@@ -73,16 +56,30 @@ def escrever(redes):
     txtDisplay.append("<span style='color:yellow'>Mascara da Rede:</span><span style='color:white;font-weight:bold'> %s</span>" % redes.netmask)
     txtDisplay.append("<span style='color:yellow'>Numero de Hosts:</span><span style='color:white;font-weight:bold'> %s</span>" % TAMANHO)
     txtDisplay.append("<span style='color:yellow'>Faixa de IP:</span><span style='color:white;font-weight:bold'>  %s <span style='color:yellow'> até </span>   %s </span>"  % (redes[1],redes[redes.__len__() - 2]))
+    txtDisplay.append("<span style='color:yellow'>Broadcast: </span><span style='color:white;font-weight:bold'>  %s " %  redes[redes.__len__() - 1])
 
     txtDisplay.append("<span style='color:#00FFFF;font-weight:bold'>:: Maquinas Online ::</span>")
     txtDisplay.append("<span style='color:red;font-weight:lighter'>===============</span>")
 
-
-
+    threads = []
     for ip in redes:
+      t = threading.Thread(target=check_online, args=(ip,))
+      threads.append(t)
+      t.start()
 
-        txtDisplay.append("Host --> %s  %s " % (ip,check_online(ip)))
 
+
+def check_online(host):
+
+    resposta = os.system('ping -n 1 {}'.format(host))
+
+    if resposta == 0:
+      pingstatus = "<span style='color:#80ff00;font-weight:bold'>Ativo  ;)</span>"
+      txtDisplay.append("Host --> %s  %s " % (host, pingstatus))
+
+    else:
+      pingstatus = "<span style='color:#ff0000;font-weight:bold'>Inativo  :(</span>"
+      txtDisplay.append("Host --> %s  %s " % (host, pingstatus))
 
 
 
@@ -130,6 +127,7 @@ def obterRedes():
   calcularRedes(inputIP.text(),inputMask.text())
 
 
+'''
 
 def getDescHost():
   txtDisplay.clear()
@@ -146,6 +144,75 @@ def getDescHost():
     cmdgen.MibVariable('iso.org.dod.internet.mgmt.mib-2.system.sysDescr.0'),
     cmdgen.MibVariable('SNMPv2-MIB', 'sysDescr', 0)
   )
+
+  # Check for errors and print out results
+  if errorIndication:
+    txtDisplay.append(str(errorIndication))
+
+  else:
+    if errorStatus:
+
+      txtDisplay.append('%s at %s' % (errorStatus.prettyPrint(), errorIndex and varBinds[int(errorIndex) - 1] or '?'))
+
+
+    else:
+      for nome, val in varBinds:
+        txtDisplay.append('%s = %s' % (nome.prettyPrint(), val.prettyPrint()))
+
+      def getDescHost():
+        txtDisplay.clear()
+
+        txtDisplay.append("<span style='color:#00FFFF;font-weight:bold'>Informação do Host</span>")
+
+        alvoTexto = inputIPAlvo.text()
+        alvo = alvoTexto[:15]
+        cmdGen = cmdgen.CommandGenerator()
+
+        errorIndication, errorStatus, errorIndex, varBinds = cmdGen.getCmd(
+          cmdgen.CommunityData('public', mpModel=0),
+          cmdgen.UdpTransportTarget((alvo, 161)),
+          cmdgen.MibVariable('iso.org.dod.internet.mgmt.mib-2.system.sysDescr.0'),
+          cmdgen.MibVariable('SNMPv2-MIB', 'sysDescr', 0)
+        )
+
+        # Check for errors and print out results
+        if errorIndication:
+          txtDisplay.append(str(errorIndication))
+
+        else:
+          if errorStatus:
+
+            txtDisplay.append(
+              '%s at %s' % (errorStatus.prettyPrint(), errorIndex and varBinds[int(errorIndex) - 1] or '?'))
+
+
+          else:
+            for nome, val in varBinds:
+              txtDisplay.append('%s = %s' % (nome.prettyPrint(), val.prettyPrint()))
+
+            '''
+
+
+def getDescHost():
+  txtDisplay.clear()
+
+
+
+
+  txtDisplay.append("<span style='color:#00FFFF;font-weight:bold'>Informação do Host</span>")
+
+  alvoTexto = inputIPAlvo.text()
+  alvo = alvoTexto[:15]
+  cmdGen = cmdgen.CommandGenerator()
+
+  errorIndication, errorStatus, errorIndex, varBinds = cmdGen.getCmd(
+    cmdgen.CommunityData('public', mpModel=0),
+    cmdgen.UdpTransportTarget((alvo, 161)),
+    cmdgen.MibVariable('iso.org.dod.internet.mgmt.mib-2.system.sysDescr.0'),
+    cmdgen.MibVariable('SNMPv2-MIB', 'sysDescr', 0)
+  )
+
+
 
   # Check for errors and print out results
   if errorIndication:

0b488707fcd8a7327cd7e09518336815dc3d1bf4
u2daniel <daniel.rodrighes@gmail.com>
2016-11-04 13:53:05 -0300

    beta version

--- main.py
+++ main.py
@@ -4,13 +4,14 @@ import sys
 import pysnmp
 import os
 import threading
+import time
 from socket import *
 from netaddr import *
 from pysnmp.entity.rfc3413.oneliner import cmdgen
 from pysnmp.hlapi import  getCmd
 
 
-
+redes_global = []
 # Definindo a Janela
 app = QApplication(sys.argv)
 janelaApp = QWidget()
@@ -46,47 +47,6 @@ txtDisplay.setStyleSheet("QTextEdit {background-color:black;color:white}")
 
 
 
-def escrever(redes):
-    txtDisplay.clear()
-
-    TAMANHO = redes.size
-
-    txtDisplay.append("<span style='color:#00FFFF;font-weight:bold'>Informações</span>")
-    txtDisplay.append("<span style='color:red;font-weight:lighter'>===============</span>")
-    txtDisplay.append("<span style='color:yellow'>Mascara da Rede:</span><span style='color:white;font-weight:bold'> %s</span>" % redes.netmask)
-    txtDisplay.append("<span style='color:yellow'>Numero de Hosts:</span><span style='color:white;font-weight:bold'> %s</span>" % TAMANHO)
-    txtDisplay.append("<span style='color:yellow'>Faixa de IP:</span><span style='color:white;font-weight:bold'>  %s <span style='color:yellow'> até </span>   %s </span>"  % (redes[1],redes[redes.__len__() - 2]))
-    txtDisplay.append("<span style='color:yellow'>Broadcast: </span><span style='color:white;font-weight:bold'>  %s " %  redes[redes.__len__() - 1])
-
-    txtDisplay.append("<span style='color:#00FFFF;font-weight:bold'>:: Maquinas Online ::</span>")
-    txtDisplay.append("<span style='color:red;font-weight:lighter'>===============</span>")
-
-    threads = []
-    for ip in redes:
-      t = threading.Thread(target=check_online, args=(ip,))
-      threads.append(t)
-      t.start()
-
-
-
-def check_online(host):
-
-    resposta = os.system('ping -n 1 {}'.format(host))
-
-    if resposta == 0:
-      pingstatus = "<span style='color:#80ff00;font-weight:bold'>Ativo  ;)</span>"
-      txtDisplay.append("Host --> %s  %s " % (host, pingstatus))
-
-    else:
-      pingstatus = "<span style='color:#ff0000;font-weight:bold'>Inativo  :(</span>"
-      txtDisplay.append("Host --> %s  %s " % (host, pingstatus))
-
-
-
-
-
-
-
 
 
 def startJanela():
@@ -117,80 +77,56 @@ def startJanela():
   hbox.addLayout(vbox2)
 
   janelaApp.setLayout(hbox)
-
-
   janelaApp.show()
-  sys.exit(app.exec_())
+
+
+def check_online(host):
+  resposta = os.system('ping -n 1 {}'.format(host))
+  time.sleep(1)
+
+  if resposta == 0:
+    pingstatus = "<span style='color:#80ff00;font-weight:bold'>Ativo  ;)</span>"
+    # txtDisplay.append("Host --> %s  %s " % (host, pingstatus))
+
+  else:
+    pingstatus = "<span style='color:#ff0000;font-weight:bold'>Inativo  :(</span>"
+    # txtDisplay.append("Host --> %s  %s " % (host, pingstatus))
+
+
+def escrever(redes):
+  txtDisplay.clear()
+
+  redes_global = redes
+
+  TAMANHO = redes.size
+
+  txtDisplay.append("<span style='color:#00FFFF;font-weight:bold'>Informações</span>")
+  txtDisplay.append("<span style='color:red;font-weight:lighter'>===============</span>")
+  txtDisplay.append(
+    "<span style='color:yellow'>Mascara da Rede:</span><span style='color:white;font-weight:bold'> %s</span>" % redes.netmask)
+  txtDisplay.append(
+    "<span style='color:yellow'>Numero de Hosts:</span><span style='color:white;font-weight:bold'> %s</span>" % TAMANHO)
+  txtDisplay.append(
+    "<span style='color:yellow'>Faixa de IP:</span><span style='color:white;font-weight:bold'>  %s <span style='color:yellow'> até </span>   %s </span>" % (
+      redes[1], redes[redes.__len__() - 2]))
+  txtDisplay.append(
+    "<span style='color:yellow'>Broadcast: </span><span style='color:white;font-weight:bold'>  %s " % redes[
+      redes.__len__() - 1])
+
+  txtDisplay.append("<span style='color:#00FFFF;font-weight:bold'>:: Maquinas Online ::</span>")
+  txtDisplay.append("<span style='color:red;font-weight:lighter'>===============</span>")
+  threads = []
+  for ip in redes_global:
+    t = threading.Thread(target=check_online, args=(ip,))
+    threads.append(t)
+    t.start()
+
 
 
 def obterRedes():
   calcularRedes(inputIP.text(),inputMask.text())
 
 
-'''
-
-def getDescHost():
-  txtDisplay.clear()
-
-  txtDisplay.append("<span style='color:#00FFFF;font-weight:bold'>Informação do Host</span>")
-
-  alvoTexto = inputIPAlvo.text()
-  alvo = alvoTexto[:15]
-  cmdGen = cmdgen.CommandGenerator()
-
-  errorIndication, errorStatus, errorIndex, varBinds = cmdGen.getCmd(
-    cmdgen.CommunityData('public', mpModel=0),
-    cmdgen.UdpTransportTarget((alvo, 161)),
-    cmdgen.MibVariable('iso.org.dod.internet.mgmt.mib-2.system.sysDescr.0'),
-    cmdgen.MibVariable('SNMPv2-MIB', 'sysDescr', 0)
-  )
-
-  # Check for errors and print out results
-  if errorIndication:
-    txtDisplay.append(str(errorIndication))
-
-  else:
-    if errorStatus:
-
-      txtDisplay.append('%s at %s' % (errorStatus.prettyPrint(), errorIndex and varBinds[int(errorIndex) - 1] or '?'))
-
-
-    else:
-      for nome, val in varBinds:
-        txtDisplay.append('%s = %s' % (nome.prettyPrint(), val.prettyPrint()))
-
-      def getDescHost():
-        txtDisplay.clear()
-
-        txtDisplay.append("<span style='color:#00FFFF;font-weight:bold'>Informação do Host</span>")
-
-        alvoTexto = inputIPAlvo.text()
-        alvo = alvoTexto[:15]
-        cmdGen = cmdgen.CommandGenerator()
-
-        errorIndication, errorStatus, errorIndex, varBinds = cmdGen.getCmd(
-          cmdgen.CommunityData('public', mpModel=0),
-          cmdgen.UdpTransportTarget((alvo, 161)),
-          cmdgen.MibVariable('iso.org.dod.internet.mgmt.mib-2.system.sysDescr.0'),
-          cmdgen.MibVariable('SNMPv2-MIB', 'sysDescr', 0)
-        )
-
-        # Check for errors and print out results
-        if errorIndication:
-          txtDisplay.append(str(errorIndication))
-
-        else:
-          if errorStatus:
-
-            txtDisplay.append(
-              '%s at %s' % (errorStatus.prettyPrint(), errorIndex and varBinds[int(errorIndex) - 1] or '?'))
-
-
-          else:
-            for nome, val in varBinds:
-              txtDisplay.append('%s = %s' % (nome.prettyPrint(), val.prettyPrint()))
-
-            '''
 
 
 def getDescHost():
@@ -228,9 +164,7 @@ def getDescHost():
       for nome, val in varBinds:
         txtDisplay.append('%s = %s' % (nome.prettyPrint(), val.prettyPrint()))
 
-# Comandos
-bntSRede.clicked.connect(obterRedes)
-bntAlvo.clicked.connect(getDescHost)
+
 
 
 
@@ -240,10 +174,16 @@ def calcularRedes(ipRede, maskRede):
     netmask = maskRede.strip('/')[:15]  # máscara /24 ou 255.255.255.0
 
     redes = IPNetwork(netaddr + '/' + netmask)
-
     escrever(redes)
 
 
 
 if __name__ == '__main__':
+
   startJanela()
+
+  # Comandos
+  bntSRede.clicked.connect(obterRedes)
+  bntAlvo.clicked.connect(getDescHost)
+
+  sys.exit(app.exec_())

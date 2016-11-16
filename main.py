9fc1e710db5c59ac216e2e24e3d51216a3a738dc
u2daniel <daniel.rodrighes@gmail.com>
2016-11-16 18:59:07 -0300

    adicionado a interface de documentos

--- main.py
+++ main.py
@@ -14,22 +14,22 @@ from pysnmp.hlapi import getCmd
 app = QApplication(sys.argv)
 
 class RedesThread(QThread):
+  def __init__(self, redes , parent=app):
 
-  def __init__(self,redes, parent=app):
+    self.lista_de_redes = redes
 
-      self.lista_de_redes = redes
+    QThread.__init__(self, parent)
 
-      QThread.__init__(self, parent)
 
-      self.signal =  SIGNAL("status")
 
+    self.signal = SIGNAL("status")
+    self.start()
 
   def run(self):
     # time.sleep(2)
 
 
     for host_ip in self.lista_de_redes:
-
 
       try:
 
@@ -55,13 +55,11 @@ class RedesThread(QThread):
         posi_tempo_esgotado = ltexto.find('esgotado')  # Resposta para Destino inacessivel
         inacessivel = ltexto[posi_inacessivel:posi_inacessivel + 7]
 
-
         tempo_esgotado = ltexto[posi_tempo_esgotado:posi_tempo_esgotado + 8]
 
+        if (inacessivel == 'inacess' or tempo_esgotado == 'esgotado'):
 
-        if( inacessivel == 'inacess' or tempo_esgotado == 'esgotado') :
-
-         self.emit(self.signal,'INATIVO',host_ip,"0")
+          self.emit(self.signal, 'INATIVO', host_ip, "0")
 
         else:
           s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -75,7 +73,7 @@ class RedesThread(QThread):
             estado_porta = 'Porta {} Fechada'.format(porta)
 
           s.close()
-          self.emit(self.signal,'ATIVO',host_ip,estado_porta)
+          self.emit(self.signal, 'ATIVO', host_ip, estado_porta)
 
 
 
@@ -83,114 +81,128 @@ class RedesThread(QThread):
 
         self.emit(self.signal, "Erro de Procssamento")
 
+class JanelaMae(QMainWindow):
+  count = 0
+
+  def __init__(self, parent=None):
+    super(JanelaMae, self).__init__(parent)
+    self.mdi = QMdiArea()
+    self.setCentralWidget(self.mdi)
+    bar = self.menuBar()
+
+    subWindBlackNight = QMdiSubWindow()
+    subWindBlackNight.setWidget(BlackNight())
+    self.mdi.addSubWindow(subWindBlackNight)
+
+    arquivo = bar.addMenu("Arquivo")
+    arquivo.addAction("Novo")
+    arquivo.addAction("Cascata")
+    arquivo.addAction("Encaixado")
+    arquivo.triggered[QAction].connect(self.windowaction)
+    self.setWindowTitle("MDI demo")
+    subWindBlackNight.show()
+
+  def windowaction(self, q):
+    print("disparou")
+
+
+    if q.text() == "Novo":
+      JanelaMae.count = JanelaMae.count + 1
+      sub = QMdiSubWindow()
+
+      sub.setWidget(QTextEdit())
+      sub.setWindowTitle("Sub Janela" + str(JanelaMae.count))
+      self.mdi.addSubWindow(sub)
+      sub.show()
+
+    if q.text() == "Cascata":
+      self.mdi.cascadeSubWindows()
+
+    if q.text() == "Encaixado":
+      self.mdi.tileSubWindows()
 
 
 
 
-class MainGui(QWidget):
+class BlackNight(QWidget):
+  def __init__(self, pai=None):
+    QWidget.__init__(self, pai)
+
+    self.STATUS_FORMATO_ATIVO = "     &#160; &#160; &#160;<span style='color:#ff0000;font-weight:bold'>&#174;</span><span style='color:#80ff00;font-weight:bold;'> ATIVO </span>"
+    self.STATUS_FORMATO_INATIVO = "     &#160; &#160; &#160;<span style='color:#ff0000;font-weight:bold'>&#8224;</span><span style='color:#ff0000;font-weight:bold;'> INATIVO </span>"
+
+    redes_global = []
+    # Definindo a Janela
 
 
-  def __init__(self,pai = None):
-      QWidget.__init__(self,pai)
+    self.setWindowTitle("BlackNight   UNIRN - BSI  Turma 2014")
+    self.setGeometry(200, 200, 820, 620)
 
+    # Entrada de Dados
+    self.inputIP = QLineEdit()
+    self.inputMask = QLineEdit()
+    self.inputIPAlvo = QLineEdit()
 
+    self.le = QLabel("Hello")
 
-      self.STATUS_FORMATO_ATIVO = "     &#160; &#160; &#160;<span style='color:#ff0000;font-weight:bold'>&#174;</span><span style='color:#80ff00;font-weight:bold;'> ATIVO </span>"
-      self.STATUS_FORMATO_INATIVO = "     &#160; &#160; &#160;<span style='color:#ff0000;font-weight:bold'>&#8224;</span><span style='color:#ff0000;font-weight:bold;'> INATIVO </span>"
+    # Comandos
+    self.bntSRede = QPushButton("Calcular Rede")
+    self.connect(self.bntSRede, SIGNAL("clicked()"), self.obterRedes)
 
+    self.bntAlvo = QPushButton("Obter Info")
 
+    self.bntArq = QPushButton("Obter Arquivo")
+    self.connect(self.bntArq, SIGNAL("clicked()"), self.lerSenhas)
 
+    self.bntSRede.setFixedWidth(150)
+    self.bntAlvo.setFixedWidth(150)
 
+    # Saida de dados
+    self.txtDisplay = QTextEdit()
+    self.txtDisplay.setFixedHeight(600)
+    self.txtDisplay.setFixedWidth(800)
+    self.txtDisplay.setStyleSheet("QTextEdit {background-color:black;color:white}")
 
-      redes_global = []
-      # Definindo a Janela
+    # Formatando e  Organizando layout
 
+    hbox = QHBoxLayout()
+    vbox1 = QVBoxLayout()
+    vbox2 = QVBoxLayout()
 
-      self.setWindowTitle("BlackNight   UNIRN - BSI  Turma 2014")
-      self.setGeometry(200, 200, 820, 620)
+    vbox1.addWidget(QLabel("Infome o IP de Rede:"))
+    vbox1.addWidget(self.inputIP)
+    vbox1.addWidget(QLabel("Mascara de Rede:"))
+    vbox1.addWidget(self.inputMask)
 
-      # Entrada de Dados
-      self.inputIP = QLineEdit()
-      self.inputMask = QLineEdit()
-      self.inputIPAlvo = QLineEdit()
+    vbox1.addWidget(self.bntSRede)
+    vbox1.addWidget(self.inputIPAlvo)
+    vbox1.addWidget(self.bntAlvo)
+    vbox1.addWidget(self.bntArq)
+    vbox1.addWidget(self.le)
+    vbox1.addStretch()
 
-      self.le = QLabel("Hello")
+    # Saida
+    vbox2.addWidget(QLabel("Saida do Processamento:"))
 
-      # Comandos
-      self.bntSRede = QPushButton("Calcular Rede")
-      self.connect(self.bntSRede,SIGNAL("clicked()"),self.obterRedes)
+    vbox2.addWidget(self.txtDisplay)
+    vbox2.addStretch()
 
-      self.bntAlvo = QPushButton("Obter Info")
+    hbox.addLayout(vbox1)
+    hbox.addLayout(vbox2)
 
-      self.bntArq = QPushButton("Obter Arquivo")
-      self.connect(self.bntArq, SIGNAL("clicked()"), self.lerSenhas)
-
-
-
-
-      self.bntSRede.setFixedWidth(150)
-      self.bntAlvo.setFixedWidth(150)
-
-      # Saida de dados
-      self.txtDisplay = QTextEdit()
-      self.txtDisplay.setFixedHeight(600)
-      self.txtDisplay.setFixedWidth(800)
-      self.txtDisplay.setStyleSheet("QTextEdit {background-color:black;color:white}")
-
-      # Formatando e  Organizando layout
-
-      hbox = QHBoxLayout()
-      vbox1 = QVBoxLayout()
-      vbox2 = QVBoxLayout()
-
-      vbox1.addWidget(QLabel("Infome o IP de Rede:"))
-      vbox1.addWidget(self.inputIP)
-      vbox1.addWidget(QLabel("Mascara de Rede:"))
-      vbox1.addWidget(self.inputMask)
-
-      vbox1.addWidget(self.bntSRede)
-      vbox1.addWidget(self.inputIPAlvo)
-      vbox1.addWidget(self.bntAlvo)
-      vbox1.addWidget(self.bntArq)
-      vbox1.addWidget(self.le)
-      vbox1.addStretch()
-
-      # Saida
-      vbox2.addWidget(QLabel("Saida do Processamento:"))
-
-      vbox2.addWidget(self.txtDisplay)
-      vbox2.addStretch()
-
-      hbox.addLayout(vbox1)
-      hbox.addLayout(vbox2)
-
-      self.setLayout(hbox)
+    self.setLayout(hbox)
 
   def lerSenhas(self):
-    arquivo = QFileDialog.getOpenFileName(self, 'Arquivo de Senhas',os.path.dirname(os.path.abspath(__file__)), "Arquivos de Texto (*.txt)")
+    arquivo = QFileDialog.getOpenFileName(self, 'Arquivo de Senhas', os.path.dirname(os.path.abspath(__file__)),
+                                          "Arquivos de Texto (*.txt)")
     print(arquivo)
 
-
     with open(arquivo, 'r') as ins:
-
       for line in ins:
         print(line)
       ins.close()
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-  def escrever(self,redes):
+  def escrever(self, redes):
     self.txtDisplay.clear()
 
     TAMANHO = redes.size
@@ -211,10 +223,6 @@ class MainGui(QWidget):
     self.txtDisplay.append("<span style='color:#00FFFF;font-weight:bold'>:: Maquinas Online ::</span>")
     self.txtDisplay.append(
       "<span style='color:red;font-weight:lighter'>=========================================================================</span>")
-
-
-
-
 
   def getDescHost(self):
     self.txtDisplay.clear()
@@ -239,23 +247,19 @@ class MainGui(QWidget):
     else:
       if errorStatus:
 
-        self.txtDisplay.append('%s at %s' % (errorStatus.prettyPrint(), errorIndex and varBinds[int(errorIndex) - 1] or '?'))
+        self.txtDisplay.append(
+          '%s at %s' % (errorStatus.prettyPrint(), errorIndex and varBinds[int(errorIndex) - 1] or '?'))
 
 
       else:
         for nome, val in varBinds:
           self.txtDisplay.append('%s = %s' % (nome.prettyPrint(), val.prettyPrint()))
 
-
-
-
-
-  def calcularRedes(self,ipRede = "127.0.0.1", maskRede = "30"):
+  def calcularRedes(self, ipRede="127.0.0.1", maskRede="30"):
     netaddr = ipRede[:15]  # endereço IP de rede
     netmask = maskRede.strip('/')[:15]  # máscara /24 ou 255.255.255.0
 
     redes = IPNetwork(netaddr + '/' + netmask)
-
 
     self.escrever(redes)
 
@@ -267,32 +271,23 @@ class MainGui(QWidget):
   def obterRedes(self):
     self.calcularRedes(self.inputIP.text(), self.inputMask.text())
 
-
-
-
-  def check_online(self,status,ip,estado_porta):
-
+  def check_online(self, status, ip, estado_porta):
 
     if status == 'INATIVO':
-      self.txtDisplay.append("HOST %s %s" % (ip,self.STATUS_FORMATO_INATIVO,))
+      self.txtDisplay.append("HOST %s %s" % (ip, self.STATUS_FORMATO_INATIVO,))
     else:
-      self.txtDisplay.append("HOST %s  %s   &#160; &#160; &#160; <span style='color:#ffffff;font-weight:bold'> %s </span>" % (ip,self.STATUS_FORMATO_ATIVO,estado_porta))
+      self.txtDisplay.append(
+        "HOST %s  %s   &#160; &#160; &#160; <span style='color:#ffffff;font-weight:bold'> %s </span>" % (
+          ip, self.STATUS_FORMATO_ATIVO, estado_porta))
 
 
 
+
+def main():
+
+    ex = JanelaMae()
+    ex.show()
+    sys.exit(app.exec_())
 
 if __name__ == '__main__':
-
-
-
-  win = MainGui()
-
-  win.show()
-
-
-
-  # Comandos
-  """
-   bntSRede.clicked.connect(obterRedes)
-   bntAlvo.clicked.connect(getDescHost)"""
-  sys.exit(app.exec_())
+    main()
